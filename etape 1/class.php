00c619d100a757ff773128c1dadc7743cc924f94
Pierre014 <pierrelroand1406@gmail.com>
2020-03-26 13:54:22 +0100

    form

--- etape 1/class.php
+++ etape 1/class.php
@@ -4,18 +4,28 @@
         private $data;
         public $surround = 'p';
 
-        public function __constructor($data= array()){
+        public function createForm($method){
+            return "<form action='#' method=$method>";
+        }
+        public function endForm(){
+            return "</form>";
+        }
+        public function __construct($data = array()){
             $this->data = $data;
         }
 
         private function surround($html){
-            return "<$this->surround>$html</$this->surround>";
+            return "<$this->surround>{$html}</$this->surround>";
         }
 
+        private function getValue($index){
+            return isset($this->data[$index])? $this->data[$index] : null;
+        }
 
         public function input($type,$name){
-
-            return $this->surround("<input type = $type name=$name>");
+            return $this->surround(
+                '<input type="'.$type.'" name="' . $name .'" value ="' . $this->getValue($name) .'">'
+            );
         }
 
         public function submit(){
--- etape 1/index.php
+++ etape 1/index.php
@@ -1,6 +1,12 @@
 <?php
 
     require 'class.php';
-    $form = new Form();
-
+    $form = new Form(array(
+        'username' => 'Pierre'
+    ));
+    echo $form -> createForm("post");
     echo $form -> input('text','username');
+    echo $form -> input('password','pass');
+    echo $form -> input('radio','gender');
+    echo $form -> submit();
+    echo $form -> endForm();
