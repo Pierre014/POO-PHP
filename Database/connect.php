cdb427472afd3c42a039a793eb66567d455af6af
pierre lorand <58699510+Pierre014@users.noreply.github.com>
2020-04-01 10:07:29 +0200

    Delete connect.php

--- Database/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    class Connect{
-
-        static public function bdd(){
-            return new pdo("mysql:host=localhost;dbname=becode;charset=utf8",'pierre','Feuille014');
-        }
-    }
